d7b3429a2bbcff96111ad79beb419b5124659511
sungbosim-33 <sungbosim5@suwon.ac.kr>
2026-03-06 11:51:00 +0900

    과

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 30F);
+            this.label1.Location = new System.Drawing.Point(271, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 50);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "심성보";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("굴림", 20F);
+            this.label2.Location = new System.Drawing.Point(274, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(229, 34);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "컴퓨터sw학과";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("굴림", 18F);
+            this.label3.Location = new System.Drawing.Point(275, 200);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(359, 30);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "sungbosim5@gmail.com";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("한컴 고딕", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.Location = new System.Drawing.Point(56, 292);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 42);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "사진 바꾸기";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("나눔스퀘어OTF Bold", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button2.Location = new System.Drawing.Point(298, 293);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(179, 41);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "git 바로가기";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("맑은 고딕", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button3.Location = new System.Drawing.Point(319, 363);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(201, 43);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "배경 바꾸기";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(32, 30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(197, 240);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,20 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+using System.Diagnostics;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections; // 1. 이게 있어야 리소스를 뒤질 수 있어요!
 
 namespace MyBusinessCard
 {
     public partial class Form1 : Form
     {
+        List<Image> myImages = new List<Image>();
+        int currentIndex = 0;
+
         public Form1()
         {
             InitializeComponent();
+            // 2. 프로그램이 켜질 때 리소스에서 사진을 가져오라고 시킵니다.
+            LoadImagesFromResources();
         }
+
+        // 3. 리소스에서 이미지만 쏙쏙 골라 장바구니에 담는 함수입니다.
+        private void LoadImagesFromResources()
+        {
+            var resourceSet = Properties.Resources.ResourceManager.GetResourceSet(System.Globalization.CultureInfo.CurrentCulture, true, true);
+            foreach (DictionaryEntry entry in resourceSet)
+            {
+                if (entry.Value is Image img)
+                {
+                    myImages.Add(img);
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            this.BackColor = Color.FromArgb(rand.Next(256), rand.Next(256), rand.Next(256));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string url = "https://github.com/sungbosim-33/CS_Week01_22017056_MyBusinessCard";
+            ProcessStartInfo psi = new ProcessStartInfo { FileName = url, UseShellExecute = true };
+            Process.Start(psi);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 사진이 하나도 안 담겼으면 오류 메시지를 띄워줍니다.
+            if (myImages.Count == 0)
+            {
+                MessageBox.Show("리소스에 등록된 사진이 없어요!");
+                return;
+            }
+
+            pictureBox1.Image = myImages[currentIndex];
+            currentIndex = (currentIndex + 1) % myImages.Count;
+        }
+
+        private void label2_Click(object sender, EventArgs e) { }
+        private void pictureBox1_Click(object sender, EventArgs e) { }
     }
 }
